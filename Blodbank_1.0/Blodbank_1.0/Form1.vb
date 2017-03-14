2334ba4c673898101a49b6c9759d0bd107dba586
emilieytt <emilie_yttis@hotmail.com>
2017-03-14 09:52:10 +0100

    Bakgrunnsfarge

--- Blodbank_1.0/Blodbank_1.0/Form1.vb
+++ Blodbank_1.0/Blodbank_1.0/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Hello World")
+        Me.BackColor = Color.Blue
     End Sub
 End Class
