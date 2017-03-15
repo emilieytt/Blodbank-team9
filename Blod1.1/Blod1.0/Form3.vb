bd07a966d874df71bc72fb4c5d7819bc5f0bccf2
aleksna <aleksandernarjord@MacBook-Air-2.local>
2017-03-15 09:21:08 +0100

    Logg inn++

---
+++ Blod1.1/Blod1.0/Form3.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form3
+    Private Sub RestartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RestartToolStripMenuItem.Click
+        'Restarter programmet
+        Application.Restart()
+    End Sub
+
+    Private Sub AvsluttToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AvsluttToolStripMenuItem.Click
+        'Avslutter programmet
+        Me.Close()
+    End Sub
+End Class
