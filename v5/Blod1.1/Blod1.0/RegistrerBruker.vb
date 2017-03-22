fdf424dd9d9b4c08dd205cea6af1cc0993eb8817
aleksna <aleksandernarjord@MacBook-Air-2.local>
2017-03-22 11:32:53 +0100

    v5

---
+++ v5/Blod1.1/Blod1.0/RegistrerBruker.vb
@@ -0,0 +1,25 @@
+﻿Public Class RegistrerBruker
+    Private Sub RegistrerBruker_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Setter bakgrunnsfarge til hvit, endrer programmets tittel, endrer farge på menylinje
+        Me.BackColor = Color.White
+        Me.Text = "Blodbanken St. Olavs"
+        MenuStrip1.BackColor = Color.Red
+
+    End Sub
+
+    Private Sub RestartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RestartToolStripMenuItem.Click
+        'Restarter programmet
+        Application.Restart()
+
+    End Sub
+
+    Private Sub AvsluttToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AvsluttToolStripMenuItem.Click
+        'Avslutter programmet
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Avbryt_knapp.Click
+        Startside.Show()
+        Me.Hide()
+    End Sub
+End Class
