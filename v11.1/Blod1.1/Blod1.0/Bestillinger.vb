9b690f94fb8c8989b46810b61c829aee31dcea84
aleksna <aleksandernarjord@MacBook-Air-2.local>
2017-03-29 14:19:49 +0200

    v11.1

---
+++ v11.1/Blod1.1/Blod1.0/Bestillinger.vb
@@ -0,0 +1,3 @@
+﻿Public Class Bestillinger
+
+End Class
---
+++ v11.1/Blod1.1/Blod1.0/Tappinger.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Tappinger
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Tilbake = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Tilbake)
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(397, 494)
+        Me.Panel1.TabIndex = 2
+        '
+        'Tilbake
+        '
+        Me.Tilbake.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Tilbake.Font = New System.Drawing.Font("Calibri Light", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Tilbake.ForeColor = System.Drawing.Color.White
+        Me.Tilbake.Location = New System.Drawing.Point(89, 405)
+        Me.Tilbake.Margin = New System.Windows.Forms.Padding(4)
+        Me.Tilbake.Name = "Tilbake"
+        Me.Tilbake.Size = New System.Drawing.Size(188, 62)
+        Me.Tilbake.TabIndex = 21
+        Me.Tilbake.Text = "Tilbake"
+        Me.Tilbake.UseVisualStyleBackColor = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.LightBlue
+        Me.ListBox1.Font = New System.Drawing.Font("Calibri Light", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.ListBox1.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 24
+        Me.ListBox1.Location = New System.Drawing.Point(23, 64)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(329, 316)
+        Me.ListBox1.TabIndex = 20
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.Label1.Location = New System.Drawing.Point(19, 25)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(241, 20)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "Dine tidligere blodtappinger"
+        '
+        'Tappinger
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(454, 545)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Tappinger"
+        Me.Text = "Tappinger"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Tilbake As Button
+End Class
